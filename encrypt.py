5a3434dde1999b0b5dd59b9ed09897ad1f00b81a
Frank E <131370932+byfranke@users.noreply.github.com>
2024-03-26 13:36:34 -0300

    encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,20 @@
 from cryptography.fernet import Fernet
 
 def encrypt_ip_port(ip_port, key_file='/tmp/.config/key', config_file='/tmp/.config/config.txt'):
-    # Generates a key and creates a Fernet instance
     key = Fernet.generate_key()
     cipher_suite = Fernet(key)
 
-    # Encrypts the data
     encrypted_data = cipher_suite.encrypt(ip_port.encode())
 
-    # Saves the key in a file
     with open(key_file, 'wb') as key_out:
         key_out.write(key)
 
-    # Saves the encrypted IP and port in the configuration file
     with open(config_file, 'wb') as config_out:
         config_out.write(encrypted_data)
 
     print("Encryption complete. Key and encrypted data have been saved.")
 
 try:
-    # Asking the user for LHOST and LPORT, or setting them directly
     lhost = input("Enter LHOST: ")
     lport = input("Enter LPORT: ")
     ip_port = f"{lhost}\n{lport}"
