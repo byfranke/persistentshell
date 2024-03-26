5bc9e2637de7bc94c7553b457a14686ed92c8136
Frank E <131370932+byfranke@users.noreply.github.com>
2024-03-26 13:40:17 -0300

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,7 +8,6 @@ SCRIPT_NAME="persistence.py"
 CONFIG_FILE="/tmp/.config/config.txt"
 KEY_FILE="/tmp/.config/key"
 
-# Stop and disable the systemd service and timer
 echo "Stopping and disabling $TIMER_NAME..."
 systemctl stop "$TIMER_NAME"
 systemctl disable "$TIMER_NAME"
@@ -16,40 +15,32 @@ echo "Stopping and disabling $SERVICE_NAME..."
 systemctl stop "$SERVICE_NAME"
 systemctl disable "$SERVICE_NAME"
 
-# Remove the systemd service and timer files
 echo "Removing systemd service and timer files..."
 rm -f "/etc/systemd/system/$SERVICE_NAME.service"
 rm -f "/etc/systemd/system/$TIMER_NAME"
 
-# Reload systemd to apply changes
 systemctl daemon-reload
 systemctl reset-failed
 
-# Remove the script, config file, and key
 echo "Removing script, configuration, and key files..."
 rm -f "$TARGET_DIR/$SCRIPT_NAME"
 rm -f "$CONFIG_FILE"
 rm -f "$KEY_FILE"
 
-# Final cleanup actions, like removing the /tmp/.config directory if it's empty
 if [ -d "/tmp/.config" ] && [ -z "$(ls -A /tmp/.config)" ]; then
     echo "Removing empty directory /tmp/.config..."
     rmdir "/tmp/.config"
 fi
 
-# Prompt to delete the current working directory
 read -p "Do you wish to delete the current directory ($PWD)? [y/n]: " del_choice
 
 case $del_choice in
     [Yy]* )
         echo "Deleting the current directory..."
-        # Move up a directory to avoid issues with deleting the current directory
         cd ..
-        # Delete the directory
         rm -rf "$PWD/persistentshell"
         echo "Directory deleted."
         
-        # Clean up command history after confirming deletion
         echo "Cleaning up terminal command history..."
         history -c && > ~/.bash_history
         echo "Command history cleaned."
